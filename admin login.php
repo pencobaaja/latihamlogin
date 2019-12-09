014663f358a7f56973f1f68f4b0d402ae6c9ae75
pencobaaja <58303958+pencobaaja@users.noreply.github.com>
2019-12-09 22:15:59 +0700

    Create admin login.php

---
+++ admin login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>admin</title>
+
+    </head>
+
+    <body>
+        <h1>selamat datang Admin</h1>
+        <a href="login.php">logout</a>
+        
+    </body>
+</html>
